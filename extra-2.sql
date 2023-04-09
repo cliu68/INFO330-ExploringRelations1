b7a08aaba2e9cd36e3d31d5dfafc703271840324
cliu68 <chunyao.liu0608@gmail.com>
2023-04-08 21:45:59 -0700

    try again

---
+++ extra-2.sql
@@ -0,0 +1,2 @@
+select distinct gender from persons;
+select count(distinct gender) AS NumGenders from persons;
---
+++ extra-3.sql
@@ -0,0 +1,4 @@
+ Select MAX(age) AS maxAge, MIN(age) AS minAge, AVG(age) AS avgAge FROM persons;
+ 
+ 
+ 
